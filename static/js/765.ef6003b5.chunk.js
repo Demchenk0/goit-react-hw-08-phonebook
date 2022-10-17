6c77b300b60c43672fd238bbfe614b05fc3f74f5
Demchenk0 <107391034+Demchenk0@users.noreply.github.com>
2022-10-17 16:44:34 +0000

    Deploying to gh-pages from @ Demchenk0/goit-react-hw-08-phonebook@25fd6e6aa33ec1d2d7208a51e8d70707f7377618 🚀

--- static/js/765.ef6003b5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[765],{9765:function(e,t,o){o.r(t),o.d(t,{Home:function(){return n}});var r=o(184);function n(){return(0,r.jsx)("div",{children:"Div4ik ili ashe4ka so slovom Home"})}}}]);
-//# sourceMappingURL=765.ef6003b5.chunk.js.map
